a8f0c4118ab2ad46c766afdf7d7575a33030a0fc
Matthew Pryor <mpryor@siue.edu>
2014-02-25 15:42:28 -0600

    Simple physics applied to ball

--- src/Animation.java
+++ src/Animation.java
@@ -1,24 +1,27 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.*;
 public class Animation
 {
- 
-public static void main(String[] args)
-{
-    	 	Animation gui=new Animation();
-    	 	gui.play();
-}
-public void play()
+	public static void main(String[] args)
+	{
+	    	 	Animation gui=new Animation();
+	    	 	gui.play();
+	}
+	public void play()
    {
            JFrame frame=new JFrame();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            DrawPanel draw=new DrawPanel();
+            DrawPanel draw = new DrawPanel();  
+            draw.addKeyListener(draw);
+            draw.setFocusable(true);            
             frame.add(draw);
             frame.setSize(500,500);
             frame.setVisible(true);
-            for(int i=0;i<150;i++)
+            while(draw.y < 500)
             {
-               draw.x++;
-               draw.y++;
+               draw.step();	
                draw.repaint();  //tells the panel to redraw itself so we can see the circle in new location             
                try {
             	   Thread.sleep(25);
--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,18 +1,43 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
-class DrawPanel extends JPanel{
-	int x = 0;
-	int y = 0;
-     public void paintComponent(Graphics g)
-       { 
-
-	      g.setColor(Color.WHITE);
-	      g.fillRect(0,0,this.getWidth(),this.getHeight());
-	      g.setColor(Color.GREEN);
-	      g.fillOval(x,y,50,50);
-       }
+class DrawPanel extends JPanel implements KeyListener{
+	int x = 246 - 25;
+	float y = 50;
+	int reverse = 1;
+	float yVelocity = 0;
+	float yAcceleration = .5f;
+	
+    public void paintComponent(Graphics g)
+    { 
+      g.setColor(Color.WHITE);
+      g.fillRect(0,0,this.getWidth(),this.getHeight());
+      g.setColor(Color.GREEN);
+      g.fillOval(x,(int)y,50,50);
+    }
+    
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyChar() == ' ')
+		{
+			yVelocity -= 20;
+					
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {	
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+	public void step()
+	{
+		yVelocity += yAcceleration;
+		y += yVelocity;
+	}
      
 }
